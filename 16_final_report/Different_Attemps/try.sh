082f7751e968c69490ba2afa7b65ecfb337e36f6
zihualiu <Luke-Liu2020@qq.com>
2021-05-27 14:07:32 +0800

    ss

--- 16_final_report/Different_Attemps/try.sh
+++ 16_final_report/Different_Attemps/try.sh
@@ -1,12 +1,20 @@
 
-# OpenMP only
+openmp_only(){
+g++  openmp_only.cpp -fopenmp  
+./a.out
+}
 
-# g++  openmp_only.cpp -fopenmp  
-# ./a.out
+openmp_mpi(){
+mpicxx openmp_mpi.cpp -fopenmp
+mpirun -np 1 ./a.out
+}
 
-# OpenMP + MPI
-# mpicxx openmp_mpi.cpp -fopenmp
-# mpirun -np 1 ./a.out
-
+openmp_mpi_simd(){
 mpicxx openmp_mpi_simd.cpp -fopenmp -fopt-info-optimized -march=native -O3
 ./a.out
+}
+cuda_only(){
+nvcc cuda_only.cu
+./a.out
+}
+cuda_only
