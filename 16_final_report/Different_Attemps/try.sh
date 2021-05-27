8877a866c820487cd9cc76baa0ed19a6a02abd8e
zihualiu <Luke-Liu2020@qq.com>
2021-05-27 13:38:11 +0800

    second

--- 16_final_report/Different_Attemps/try.sh
+++ 16_final_report/Different_Attemps/try.sh
@@ -1,7 +1,12 @@
 
+# OpenMP only
+
 # g++  openmp_only.cpp -fopenmp  
 # ./a.out
 
-mpicxx openmp_mpi.cpp -fopenmp
-mpirun -np 1 ./a.out
+# OpenMP + MPI
+# mpicxx openmp_mpi.cpp -fopenmp
+# mpirun -np 1 ./a.out
 
+mpicxx openmp_mpi_simd.cpp -fopenmp -fopt-info-optimized -march=native -O3
+./a.out
